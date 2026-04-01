0eb54815337a695056c89c04b7fe9cc0b2e5bdcf
NAMPV <pvnam198@gmail.com>
2026-04-01 11:06:19 +0700

    init

---
+++ iaptestapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iaptestapp
+//
+//  Created by Phạm Văn Nam on 1/4/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iaptestappTests/iaptestappTests.swift
@@ -0,0 +1,17 @@
+//
+//  iaptestappTests.swift
+//  iaptestappTests
+//
+//  Created by Phạm Văn Nam on 1/4/26.
+//
+
+import Testing
+@testable import iaptestapp
+
+struct iaptestappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
